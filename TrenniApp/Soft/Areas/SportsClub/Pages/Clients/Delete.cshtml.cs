95b6c84402c8248b486d36d08a8fe3cbcb3566b0
Erle <maido.erle@gmail.com>
2020-05-03 21:39:30 +0300

    Clients scaffolding

---
+++ TrenniApp/Soft/Areas/SportsClub/Pages/Clients/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TrainingApp.Facade.SportsClub;
+using TrainingApp.Soft.Data;
+
+namespace TrainingApp.Soft.Areas.SportsClub.Pages.Clients
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly TrainingApp.Soft.Data.ApplicationDbContext _context;
+
+        public DeleteModel(TrainingApp.Soft.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public ClientView ClientView { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ClientView = await _context.ClientView.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (ClientView == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ClientView = await _context.ClientView.FindAsync(id);
+
+            if (ClientView != null)
+            {
+                _context.ClientView.Remove(ClientView);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
