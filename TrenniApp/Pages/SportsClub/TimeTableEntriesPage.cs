507d6b40d3a51523cfbf8851fc6986a5e1d2ea69
Mark Matteus Murru <markmatteus12@gmail.com>
2020-05-09 17:38:01 +0300

    pisut edasi tehtud

--- TrenniApp/Pages/SportsClub/TimeTableEntriesPage.cs
+++ TrenniApp/Pages/SportsClub/TimeTableEntriesPage.cs
@@ -7,7 +7,20 @@ using TrainingApp.Facade.SportsClub;
 
 namespace TrainingApp.Pages.SportsClub
 {
-    //public abstract class TimeTableEntriesPage : CommonPage<ITimetableEntriesRepository, TimetableEntry, TimetableEntryView, TimetableEntryData>
-    //{
-    //}
+    public abstract class TimeTableEntriesPage : CommonPage<ITimetableEntriesRepository, TimetableEntry,
+        TimetableEntryView, TimetableEntryData>
+    {
+        protected internal readonly IParticipantsOfTrainingRepository participants;
+
+        protected internal TimeTableEntriesPage(ITimetableEntriesRepository e, IParticipantsOfTrainingRepository p,
+            ILocationsRepository l, ICoachesRepository c, ITrainingsRepository t, ITrainingTypesRepository s) :  base(e)
+        {
+            PageTitle = "Timetable Entries";
+            Participants = new List<ParticipantOfTrainingView>();
+
+        }
+
+        public IList<ParticipantOfTrainingView> Participants { get;}
+
+    }
 }
