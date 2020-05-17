96d28595fb54bd696dbf83f49b93fbc90c87db04
johannkuldm2e <38594359+johannkuldm2e@users.noreply.github.com>
2020-05-17 14:35:26 +0300

    TimeTableEntries Select function fix

--- TrenniApp/Soft/Areas/SportsClub/Pages/TimetableEntries/Create.cshtml.cs
+++ TrenniApp/Soft/Areas/SportsClub/Pages/TimetableEntries/Create.cshtml.cs
@@ -17,7 +17,8 @@ namespace TrainingApp.Soft.Areas.SportsClub.Pages.TimetableEntries
     {
 
         public CreateModel(ITimetableEntriesRepository r, IParticipantOfTrainingsRepository p, 
-            ITrainingsRepository t, ICoachesRepository c, ILocationsRepository l, ITrainingTypesRepository tt) : base(r, p, t, c, l, tt)
+            ITrainingsRepository t, ICoachesRepository c, ILocationsRepository l, ITrainingTypesRepository tt,
+            IClientsRepository cl) : base(r, p, t, c, l, tt, cl)
         {
         }
 
