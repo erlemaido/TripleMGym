fe6fd39a4801e914a439982e624cfbc9672c41fe
Mark Matteus Murru <markmatteus12@gmail.com>
2020-05-20 12:37:23 +0300

    Parandasin EntityTesti meie enda lõpliku abc koodi järgi, mitte piho lõpliku abc. Kommenteerisin Johanni oma välja, ära veel ei kustutanud

--- TrenniApp/Tests/Domain/Common/EntityTests.cs
+++ TrenniApp/Tests/Domain/Common/EntityTests.cs
@@ -9,67 +9,102 @@ using TrainingApp.Domain.Common;
 namespace TrainingApp.Tests.Domain.Common
 {
     [TestClass]
-    public class EntityTests : AbstractClassTests<Entity<TimetableEntryData>, object>
+    public class EntityTests : AbstractClassTests<Entity<ClientData>, object>
     {
-        private class TestClass : Entity<TimetableEntryData>
-        {
+        private class TestClass : Entity<ClientData>
+            {
+                public TestClass(ClientData d = null) : base(d) { }
+            }
 
-            public TestClass(TimetableEntryData d = null) : base(d) { }
+            [TestInitialize]
+            public override void TestInitialize()
+            {
+                base.TestInitialize();
+                obj = new TestClass();
+            }
 
-        }
+            [TestMethod]
+            public void DataTest()
+            {
+                var d = GetRandom.Object<ClientData>();
+                Assert.AreNotSame(d, obj.Data);
+                obj = new TestClass(d);
+                Assert.AreSame(d, obj.Data);
+            }
+            [TestMethod]
+            public void DataIsNullTest()
+            {
+                var d = GetRandom.Object<ClientData>();
+                Assert.IsNull(obj.Data);
+                obj.Data = d;
+                Assert.AreSame(d, obj.Data);
+            }
+            [TestMethod]
+            public void CanSetNullDataTest()
+            {
+                obj.Data = null;
+                Assert.IsNull(obj.Data);
+            }//private class TestClass : Entity<TimetableEntryData>
+             //{
 
-        [TestInitialize]
-        public override void TestInitialize()
-        {
-            base.TestInitialize();
-            obj = new TestClass();
-        }
+            //    public TestClass(TimetableEntryData d = null) : base(d) { }
 
-        [TestMethod]
-        public void DataTest()
-        {
-            isReadOnlyProperty(obj, nameof(obj.Data), null);
-        }
+            //}
 
-        //[TestMethod]
-        //public void IsUnspecifiedTest()
-        //{
-        //    Assert.IsTrue(new testClass().IsUnspecified);
-        //    Assert.IsFalse(new testClass(GetRandom.Object<TimetableEntryData>()).IsUnspecified);
-        //}
+            //[TestInitialize]
+            //public override void TestInitialize()
+            //{
+            //    base.TestInitialize();
+            //    obj = new TestClass();
+            //}
 
-        [TestMethod]
-        public void CanSetDataTest()
-        {
-            var d = GetRandom.Object<TimetableEntryData>();
-            obj = new TestClass(d);
-            Assert.AreNotSame(d, obj.Data);
-            arePropertiesEqual(d, obj.Data);
-        }
+            //[TestMethod]
+            //public void DataTest()
+            //{
+            //    isReadOnlyProperty(obj, nameof(obj.Data), null);
+            //}
 
-        [TestMethod]
-        public void CanSetNullDataTest()
-        {
-            obj = new TestClass();
-            Assert.IsNull(obj.Data);
-        }
+            ////[TestMethod]
+            ////public void IsUnspecifiedTest()
+            ////{
+            ////    Assert.IsTrue(new testClass().IsUnspecified);
+            ////    Assert.IsFalse(new testClass(GetRandom.Object<TimetableEntryData>()).IsUnspecified);
+            ////}
 
-        [TestMethod]
-        public void CantChangeDataElementsTest()
-        {
-            obj = new TestClass(GetRandom.Object<TimetableEntryData>());
-            var d = obj.Data;
-            obj.Data.Id = GetRandom.String();
-            obj.Data.Description = GetRandom.String();
-            obj.Data.StartTime = GetRandom.DateTime();
-            obj.Data.EndTime = GetRandom.DateTime();
-            obj.Data.CoachId = GetRandom.String();
-            obj.Data.TrainingId = GetRandom.String();
-            obj.Data.LocationId = GetRandom.String();
-            obj.Data.TrainingTypeId = GetRandom.String();
-            //obj.Data.TrainingLevel GetRandomisse ei saa TrainingLevelit lisada
-            obj.Data.MaxNumberOfParticipants = GetRandom.Int32();
-            arePropertiesEqual(d, obj.Data);
-        }
-    }
+            //[TestMethod]
+            //public void CanSetDataTest()
+            //{
+            //    var d = GetRandom.Object<TimetableEntryData>();
+            //    obj = new TestClass(d);
+            //    Assert.AreNotSame(d, obj.Data);
+            //    arePropertiesEqual(d, obj.Data);
+            //}
+
+            //[TestMethod]
+            //public void CanSetNullDataTest()
+            //{
+            //    obj = new TestClass();
+            //    Assert.IsNull(obj.Data);
+            //}
+
+            //[TestMethod]
+            //public void CantChangeDataElementsTest()
+            //{
+            //    obj = new TestClass(GetRandom.Object<TimetableEntryData>());
+            //    var d = obj.Data;
+            //    obj.Data.Id = GetRandom.String();
+            //    obj.Data.Description = GetRandom.String();
+            //    obj.Data.StartTime = GetRandom.DateTime();
+            //    obj.Data.EndTime = GetRandom.DateTime();
+            //    obj.Data.CoachId = GetRandom.String();
+            //    obj.Data.TrainingId = GetRandom.String();
+            //    obj.Data.LocationId = GetRandom.String();
+            //    obj.Data.TrainingTypeId = GetRandom.String();
+            //    //obj.Data.TrainingLevel GetRandomisse ei saa TrainingLevelit lisada
+            //    obj.Data.MaxNumberOfParticipants = GetRandom.Int32();
+            //    arePropertiesEqual(d, obj.Data);
+            //}
+        
+}
+
 }
