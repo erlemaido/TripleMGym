ba8000d12f4d5a72e073e95e5573816796f5b175
Mark Matteus Murru <markmatteus12@gmail.com>
2020-05-21 13:02:00 +0300

    Facade Testid tehtud! tuleks kindlasti üle vaadata, kas peabki nii lihtne olema

---
+++ TrenniApp/Tests/Facade/SportsClub/TimetableEntryViewFactoryTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrainingApp.Data.SportsClub;
+using TrainingApp.Domain.SportsClub;
+using TrainingApp.Facade.SportsClub;
+using TrainingApp.Aids;
+namespace TrainingApp.Tests.Facade.SportsClub
+{
+    [TestClass]
+    public class TimetableEntryViewFactoryTests : BaseTests
+    {
+        [TestInitialize]
+        public virtual void TestInitialize()
+        {
+            type = typeof(TimetableEntryViewFactory);
+        }
+        [TestMethod]
+        public void CreateTest()
+        {
+        }
+        [TestMethod]
+        public void CreateObjectTest()
+        {
+            var view = GetRandom.Object<TimetableEntryView>();
+            var data = TimetableEntryViewFactory.Create(view).Data;
+            testArePropertyValuesEqual(view, data);
+
+        }
+        [TestMethod]
+        public void CreateViewTest()
+        {
+            var data = GetRandom.Object<TimetableEntryData>();
+            var view = TimetableEntryViewFactory.Create(new TimetableEntry(data));
+            testArePropertyValuesEqual(view, data);
+
+        }
+    }
+}
