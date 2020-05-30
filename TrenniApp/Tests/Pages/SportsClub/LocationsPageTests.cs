f49853ba6a4eaac620929d4d5f4dc2bb3a5b7631
johannkuldm2e <38594359+johannkuldm2e@users.noreply.github.com>
2020-05-30 11:44:24 +0300

    LocationsPage tests

--- TrenniApp/Tests/Pages/SportsClub/LocationsPageTests.cs
+++ TrenniApp/Tests/Pages/SportsClub/LocationsPageTests.cs
@@ -14,66 +14,42 @@ namespace TrainingApp.Tests.Pages.SportsClub
 {
     [TestClass]
     public class LocationsPageTests : AbstractClassTests<LocationsPage,
-        CommonPage<ILocationsRepository, Location, CoachView, CoachData>>
+        CommonPage<ILocationsRepository, Location, LocationView, LocationData>>
     {
         private class TestClass : LocationsPage
         {
-            internal TestClass(ILocationsRepository c, ITimetableEntriesRepository te, ITrainingsRepository t, ILocationsRepository l, ITrainingTypesRepository tt) : base(c, te, t, l, tt)
+            internal TestClass(ILocationsRepository l) : base(l)
             {
             }
         }
 
-        private class TestRepository : BaseTestRepositoryForUniqueEntity<Location, CoachData>, ILocationsRepository { }
-        private class TestTrainingTypesRepository : BaseTestRepositoryForUniqueEntity<TrainingType, TrainingTypeData>, ITrainingTypesRepository { }
+        private class TestRepository : BaseTestRepositoryForUniqueEntity<Location, LocationData>, ILocationsRepository { }
+      
+        private TestRepository locations;
+        private LocationData data;
 
-        private class TestTimetableEntriesRepository : BaseTestRepositoryForUniqueEntity<TimetableEntry, TimetableEntryData>, ITimetableEntriesRepository { }
-        private class TestLocationsRepository : BaseTestRepositoryForUniqueEntity<Location, LocationData>, ILocationsRepository { }
-        private class TestTrainingsRepository : BaseTestRepositoryForUniqueEntity<Training, TrainingData>, ITrainingsRepository { }
-
-        private TestRepository coaches;
-        private TestTrainingTypesRepository types;
-        private TestTimetableEntriesRepository entries;
-        private TestLocationsRepository locations;
-        private CoachData data;
-        private TrainingData trainingData;
-        private TrainingTypeData trainingTypeData;
-        private LocationData locationData;
-        private TestTrainingsRepository trainings;
 
 
         [TestInitialize]
         public override void TestInitialize()
         {
             base.TestInitialize();
-            coaches = new TestRepository();
-            entries = new TestTimetableEntriesRepository();
-            trainings = new TestTrainingsRepository();
-            locations = new TestLocationsRepository();
-            types = new TestTrainingTypesRepository();
-            data = GetRandom.Object<CoachData>();
-            var c = new Location(data);
-            coaches.Add(c).GetAwaiter();
-            trainingData = GetRandom.Object<TrainingData>();
-            var t = new Training(trainingData);
-            trainings.Add(t).GetAwaiter();
-            trainingTypeData = GetRandom.Object<TrainingTypeData>();
-            var tt = new TrainingType(trainingTypeData);
-            types.Add(tt).GetAwaiter();
-            locationData = GetRandom.Object<LocationData>();
-            var l = new Location(locationData);
+            locations = new TestRepository();
+            data = GetRandom.Object<LocationData>();
+            var l = new Location(data);
             locations.Add(l).GetAwaiter();
-            obj = new TestClass(coaches, entries, trainings, locations, types);
+            obj = new TestClass(locations);
         }
         [TestMethod]
         public void ItemIdTest()
         {
-            var item = GetRandom.Object<CoachView>();
+            var item = GetRandom.Object<LocationView>();
             obj.Item = item;
             Assert.AreEqual(item.GetId(), obj.ItemId);
         }
 
         [TestMethod]
-        public void PageTitleTest() => Assert.AreEqual("Treenerid", obj.PageTitle);
+        public void PageTitleTest() => Assert.AreEqual("Asukohad", obj.PageTitle);
 
         [TestMethod]
         public void PageUrlTest() => Assert.AreEqual("/SportsClub/Locations", obj.PageUrl);
@@ -81,7 +57,7 @@ namespace TrainingApp.Tests.Pages.SportsClub
         [TestMethod]
         public void ToObjectTest()
         {
-            var view = GetRandom.Object<CoachView>();
+            var view = GetRandom.Object<LocationView>();
             var o = obj.ToObject(view);
             TestArePropertyValuesEqual(view, o.Data);
         }
@@ -89,67 +65,9 @@ namespace TrainingApp.Tests.Pages.SportsClub
         [TestMethod]
         public void ToViewTest()
         {
-            var d = GetRandom.Object<CoachData>();
+            var d = GetRandom.Object<LocationData>();
             var view = obj.ToView(new Location(d));
             TestArePropertyValuesEqual(view, d);
-        }
-
-        [TestMethod]
-        public void TimetableEntriesTest()
-        {
-            var list = entries.Get().GetAwaiter().GetResult();
-            Assert.AreEqual(list.Count, obj.TimetableEntries.Count());
-        }
-
-        [TestMethod]
-        public void TrainingsTest()
-        {
-            var list = trainings.Get().GetAwaiter().GetResult();
-            Assert.AreEqual(list.Count, obj.Trainings.Count());
-        }
-
-        [TestMethod]
-        public void LocationsTest()
-        {
-            var list = locations.Get().GetAwaiter().GetResult();
-            Assert.AreEqual(list.Count, obj.Locations.Count());
-        }
-
-        [TestMethod]
-        public void TrainingTypesTest()
-        {
-            var list = types.Get().GetAwaiter().GetResult();
-            Assert.AreEqual(list.Count, obj.TrainingTypes.Count());
-        }
-
-        [TestMethod]
-        public void GetTrainingNameTest()
-        {
-            var name = obj.GetTrainingName(trainingData.Id);
-            Assert.AreEqual(trainingData.Name, name);
-        }
-
-        [TestMethod]
-        public void GetTrainingTypeNameTest()
-        {
-            var name = obj.GetTrainingTypeName(trainingTypeData.Id);
-            Assert.AreEqual(trainingTypeData.Name, name);
-        }
-
-        [TestMethod]
-        public void GetLocationNameTest()
-        {
-            var name = obj.GetLocationName(locationData.Id);
-            Assert.AreEqual(locationData.Name, name);
-        }
-
-        [TestMethod]
-        public void LoadDetailsTest()
-        {
-            var list = entries.Get().GetAwaiter().GetResult();
-            var item = GetRandom.Object<CoachView>();
-            obj.LoadDetails(item);
-            Assert.AreNotEqual(list, obj.timetableEntries);
         }
     }
 }
